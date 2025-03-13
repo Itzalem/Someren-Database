daaf84613966ec4318ec1d0d63ba35decd9d54ee
Itzalem <melissa.lj04@gmail.com>
2025-03-13 11:18:35 +0100

    Student Model Created

---
+++ Someren Database/Models/Student.cs
@@ -0,0 +1,31 @@
+﻿namespace Someren_Database.Models
+{
+    public class Student
+    {
+        public int StudentNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int PhoneNumber { get; set; }
+        public string StudentClass { get; set; }
+
+        public string RoomNumber { get; set; }
+
+        public int NumberOfStudents { get; set; }
+
+        public Student ()
+        {
+        }
+
+        public Student (int studentNumber, string firstName, string lastName, int phoneNumber, 
+            string studentClass, string roomNumber, int numberOfStudents)
+        {
+            StudentNumber = studentNumber;
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            StudentClass = studentClass;
+            RoomNumber = roomNumber;
+            NumberOfStudents = numberOfStudents;
+        }
+    }
+}
