68ca6adb10c141b181819c6196ceae6d3ad63f9b
Itzalem <melissa.lj04@gmail.com>
2025-03-27 10:44:27 +0100

    student and drink list

--- Someren Database/Controllers/DrinksController.cs
+++ Someren Database/Controllers/DrinksController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Someren_Database.Models;
+using Someren_Database.ViewModels;
 using Someren_Database.Repositories;
 
 namespace Someren_Database.Controllers
@@ -9,16 +10,26 @@ namespace Someren_Database.Controllers
     {
 
 		private readonly IDrinksRepository _drinksRepository;
+		private readonly IStudentsRepository _studentsRepository;
 
-		public DrinksController(IDrinksRepository drinksRepository)
+		public DrinksController(IDrinksRepository drinksRepository, IStudentsRepository studentsRepository)
 		{
 			_drinksRepository = drinksRepository;
+			_studentsRepository = studentsRepository;
 		}
 
-		public IActionResult DrinksIndex()
+		public IActionResult DrinksIndex(string lastNameFilter = null)
         {
 			List<Drink> drinks = _drinksRepository.ListDrinks();
-			return View(drinks);
+			List<Student> students = _studentsRepository.ListStudents(lastNameFilter);
+
+			Drink_StudentViewModel viewModel = new Drink_StudentViewModel
+			{
+				Drinks = drinks,
+				Students = students
+			};
+
+			return View(viewModel);
         }
     }
 }
